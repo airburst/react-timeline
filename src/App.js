022fde413c197cb1cb44b3187038c8f4f52468dd
airburst <mark@fairhursts.net>
2016-12-01 17:48:39 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <svg>
+        <circle cx={50} cy={50} r={10} fill="red" />
+      </svg>
+    )
+  }
+}
+
+export default App;
